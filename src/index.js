6bb6b48dbbeadf684baecb6ee246b1a1d923cf74
kimdongyoung <wwweric12@naver.com>
2023-08-01 17:57:40 +0900

    setting : 파일구조 설정

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,5 +10,3 @@ root.render(
     <App />
   </React.StrictMode>,
 );
-
-reportWebVitals();
